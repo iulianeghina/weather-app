75631f227dd3b4e7ba9cc1c5dd6d3b8e8b0525a6
iulianeghina <102993233+iulianeghina@users.noreply.github.com>
2022-06-13 19:43:35 +0200

    Weather forecast / API Integration

--- src/script.js
+++ src/script.js
@@ -33,6 +33,15 @@ currentDayAndTime.innerHTML = `${
 let actualDate = document.querySelector("#actual-date");
 actualDate.innerHTML = `${date}/${month}/${year}`;
 
+function showWeatherForecast(coordinates) {
+  let apiKey = "e0627f6356ddf3111af3ae2f46c9bf52";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrlCurrentDayForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric&cnt=8`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(commingDaysForecast);
+  axios.get(apiUrl).then(curretDayForecast);
+}
+
 function showWeatherInSearchedCity(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -61,6 +70,7 @@ function showWeatherInSearchedCity(response) {
   let wind = document.querySelector("#wind");
   let roundedWindSpeed = Math.round(response.data.wind.speed);
   wind.innerHTML = `wind ${roundedWindSpeed}km/h`;
+  showWeatherForecast(response.data.coord);
 }
 
 function searchWeather(event) {
@@ -74,35 +84,89 @@ function searchWeather(event) {
 let searchCity = document.querySelector("#search-city");
 searchCity.addEventListener("submit", searchWeather);
 
-function curretDayForecast() {
+function curretDayForecast(response) {
+  let forecast = response.data.daily;
   let dayInterval = document.querySelector("#day-interval");
   let dayForecastHTML = `<div class="row">`;
-  let intervals = ["Night", "Morning", "Midday", "Evenig"];
-  intervals.forEach(function (interval) {
-    dayForecastHTML =
-      dayForecastHTML +
-      `
+  dayForecastHTML =
+    dayForecastHTML +
+    `
        <div class="col-3 day-interval">
-        <div class="night">${interval}</div>
-        <img src="http://openweathermap.org/img/wn/01n@2x.png" alt="clar-sky-night" width="35px">
-        <div class="night-temperature">5°</div>
+        <div class="night">Morning</div>
+        <div class = "interval-temperature"> 🌡 </div>
+        <div class="over-the-day-temperature">${Math.round(
+          forecast[0].temp.morn
+        )}°</div>
         </div>`;
-  });
+  dayForecastHTML =
+    dayForecastHTML +
+    `
+       <div class="col-3 day-interval">
+        <div class="night">Midday</div>
+        <div class = "interval-temperature"> 🌡 </div>
+        <div class="over-the-day-temperature">${Math.round(
+          forecast[0].temp.day
+        )}°</div>
+        </div>`;
+  dayForecastHTML =
+    dayForecastHTML +
+    `
+       <div class="col-3 day-interval">
+        <div class="night">Evening</div>
+        <div class = "interval-temperature"> 🌡 </div>
+        <div class="over-the-day-temperature">${Math.round(
+          forecast[0].temp.eve
+        )}°</div>
+        </div>`;
+  dayForecastHTML =
+    dayForecastHTML +
+    `
+       <div class="col-3 day-interval">
+        <div class="night">Night</div>
+        <div class = "interval-temperature"> 🌡 </div>
+        <div class="over-the-day-temperature">${Math.round(
+          forecast[0].temp.night
+        )}°</div>
+        </div>`;
   dayInterval.innerHTML = dayForecastHTML;
   dayForecastHTML = dayForecastHTML + `</div>`;
 }
 
-function commingDaysForecast() {
+function formatDayForecast(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Thusday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+
+function commingDaysForecast(response) {
+  console.log(response.data);
+  let forecast = response.data.daily;
   let nextDayForecast = document.querySelector("#comming-days-forecast");
   let commmingDaysForecastHTML = `<div>`;
-  let days = ["Tomorrow", "Monday", "Thusday", "Wednesday", "Thursday"];
-  days.forEach(function (day) {
-    commmingDaysForecastHTML =
-      commmingDaysForecastHTML +
-      `<li class="days">${day}
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 6) {
+      commmingDaysForecastHTML =
+        commmingDaysForecastHTML +
+        `<li class="days">${formatDayForecast(forecastDay.dt)}
                     <br />
-                    <p> <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="clar-sky-night" width="35px"> 16°</p>
+                    <p> <img src="http://openweathermap.org/img/wn/${
+                      forecastDay.weather[0].icon
+                    }@2x.png" alt=${
+          forecastDay.weather[0].description
+        } width="35px"> <span>${Math.round(
+          forecastDay.temp.max
+        )}°</span> / ${Math.round(forecastDay.temp.min)}°</p>
                   </li>`;
+    }
   });
   nextDayForecast.innerHTML = commmingDaysForecastHTML;
   commmingDaysForecastHTML = commmingDaysForecastHTML + `</div>`;
@@ -139,6 +203,7 @@ function showDefaultWeather(city) {
 }
 
 function showCurrentLocationTemperature(event) {
+  event.preventDefault();
   function showPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
@@ -154,5 +219,3 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", showCurrentLocationTemperature);
 
 showDefaultWeather("Brussel");
-curretDayForecast();
-commingDaysForecast();
