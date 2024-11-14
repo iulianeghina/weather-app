f03e6df75a919b2299d9f70141af7733e6875bcf
Iulia Neghina <iulia.madalina.neghina@gmail.com>
2024-11-14 12:42:10 +0100

    Update script.js
    One Call API 2.5 to One Call API 3.0

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 function showWeatherForecast(coordinates) {
   let apiKey = "faa261b304bfc269bca49770138629cd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(commingDaysForecast);
   axios.get(apiUrl).then(curretDayForecast);
 }
